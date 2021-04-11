c5de512624dcd4a5ccdb02fc2871e2dfbbda23c9
Benlewis9000 <benjaminlewis9000@gmail.com>
2021-04-11 22:15:28 +0100

    Commit resources.

--- src/main/java/github/benlewis9000/LearnSpring/LearnSpringApplication.java
+++ src/main/java/github/benlewis9000/LearnSpring/LearnSpringApplication.java
@@ -1,7 +1,11 @@
 package github.benlewis9000.LearnSpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import java.util.List;
 
@@ -25,6 +31,27 @@ public class LearnSpringApplication {
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
+	}
+
+	@GetMapping("/")
+	public String index(){
+		return "index";
+	}
+
+	@Autowired
+	private ThymeleafProperties properties;
+
+	@Value("${spring.thymeleaf.templates_root:}")
+	private String templatesRoot;
+
+	@Bean
+	public ITemplateResolver defaultTemplateResolver() {
+		FileTemplateResolver resolver = new FileTemplateResolver();
+		resolver.setSuffix(properties.getSuffix());
+		resolver.setPrefix(templatesRoot);
+		resolver.setTemplateMode(properties.getMode());
+		resolver.setCacheable(properties.isCache());
+		return resolver;
 	}
 
 	/*
